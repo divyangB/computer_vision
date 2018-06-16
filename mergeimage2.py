35f05caf45ba2ce65b64bd897dfdab9cf8645f6d
Divyang bhargava <divyang018@gmail.com>
2018-06-17 00:26:42 +0530

    merging images after bg removal

---
+++ mergeimage2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import cv2
+import numpy as np
+img1 = cv2.imread('1.jpg',1)  #road
+img2 = cv2.imread('2.jpg',1)  #car
+img2_gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+ret, mask = cv2.threshold(img2_gray,252,255,cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+# masking the road
+road = cv2.bitwise_and(img1,img1,mask =mask)
+# car with no background
+car = cv2.bitwise_and(img2,img2,mask = mask_inv)
+# adding road bg and car wwith no bg
+result = cv2.add(road,car)
+
+# displaying images
+cv2.imshow("img1",img1)
+cv2.imshow("road background",road)
+cv2.imshow('mask',mask)
+cv2.imshow('mask_inc',mask_inv)
+cv2.imshow('car no bg',car)
+cv2.imshow('Result',result)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
